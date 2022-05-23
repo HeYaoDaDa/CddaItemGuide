99ded7367329acffeb1081ea97a28da6831a6021
HeYaoDaDa <37257891+HeYaoDaDa@users.noreply.github.com>
2022-05-23 20:37:00 +0800

    add go home router

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> <router-link to="/">Quasar App</router-link> </q-toolbar-title>
 
         <search-input />
       </q-toolbar>
