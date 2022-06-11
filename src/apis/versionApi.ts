402bb56cf5d1e2bb2276b69c741088c885a2ed7d
wilson <3050166231@qq.com>
2022-06-11 12:28:01 +0800

    add dummy versions

--- src/apis/versionApi.ts
+++ src/apis/versionApi.ts
@@ -1,8 +1,10 @@
 import { api } from 'boot/axios';
-import { API_VERSION, HOST } from 'src/constants/apiConstant';
+// import { API_VERSION, HOST } from 'src/constants/apiConstant';
 import { Version } from 'src/types/Version';
 
 export async function getVersions(): Promise<Version[]> {
-  const response = await api.get(`${HOST}/${API_VERSION}/versions`);
+  // const response = await api.get(`${HOST}/${API_VERSION}/versions`);
+  //TODO: Dummy url
+  const response = await api.get('https://raw.githubusercontent.com/HeYaoDaDa/CddaFile/master/versions.json');
   return response.data;
 }
